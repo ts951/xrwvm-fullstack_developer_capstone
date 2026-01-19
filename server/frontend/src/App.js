4cdc380510e8a468d77e276cfb5c7ebc56053585
Thomas Squires <thomassquires951@gmail.com>
2026-01-19 13:28:19 -0500

    Fix some issues with index.html not showing up with /login/ and /register/

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,5 @@
-import LoginPanel from "./components/Login/Login"
-import RegisterPanel from "./components/Register/Register"
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
