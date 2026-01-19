94f402e0c3717caf3836718794b512bbe031ebec
Thomas Squires <thomassquires951@gmail.com>
2026-01-19 12:25:30 -0500

    Quick fixes on CarMake and CarModel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,15 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Create your models here.
-
-class CarMake(models.model):
+class CarMake(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
-class CarModel(models.model):
+class CarModel(models.Model):
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     types = models.CharField(max_length=50, 
