a8a1a07e5dbd4e93b86457119c9c024ba7dadb27
Thomas Squires <thomassquires951@gmail.com>
2026-01-22 11:37:42 -0500

    Just some more linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,11 +10,12 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -23,8 +24,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
