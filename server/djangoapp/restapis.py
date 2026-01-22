0a13386107fab28a2d4471e8ea5a121146099051
Thomas Squires <thomassquires951@gmail.com>
2026-01-22 11:38:33 -0500

    Undo change to exception in get_request function

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -24,8 +24,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+    except:
+        # If any error occurs
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
